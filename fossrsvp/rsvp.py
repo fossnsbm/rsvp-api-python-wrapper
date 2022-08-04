a0a19c0b529c211eb12f0890c4eb83da5b5b5e78
z9fr <z9fr@protonmail.com>
2022-08-05 03:04:56 +0530

    feat: wrapper functions for events

--- fossrsvp/rsvp.py
+++ fossrsvp/rsvp.py
@@ -2,5 +2,33 @@ from . import session
 
 
 class Fossrsvp(object):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, base="https://rsvp-api.fossnsbm.org/api/v1/"):
+        self.base = base
+
+    def _get(self, endpoint: str, extension=None, slug=None):
+        url = f"{self.base}{endpoint}"
+
+        if extension is not None:
+            url += f"/{extension}"
+
+        if slug is not None:
+            url += f"/{slug}"
+
+        resp = session.get(url)
+        return resp.json()
+
+    """
+    Get all the events
+    /api/v1/events/
+    """
+
+    def events(self, slug=None, extension=None):
+        return self._get(f"events", slug=slug, extension=extension)
+
+    """
+    Get event details by slug
+    /api/v1/event/slug
+    """
+
+    def event(self, slug: str, extension="info"):
+        return self._get(f"event", slug=slug, extension=extension)
--- tests/test_rsvp.py
+++ tests/test_rsvp.py
@@ -3,5 +3,11 @@ from fossrsvp import Fossrsvp
 service = Fossrsvp()
 
 
-def test_events():
-    pass
+def test_fetch_events():
+    resp = service.events()
+    print(resp)
+
+
+def test_fetch_one_event():
+    resp = service.event(slug="shift-into-foss")
+    print(resp)
